55afd9ad9b130cff4dff0739af293d2d2bef3f31
mdshahadad <identity.shahadad@gmail.com>
2024-12-13 21:47:58 +0600

    Added navbar

--- src/Components/HomeComponent/Navbar/Navbar.jsx
+++ src/Components/HomeComponent/Navbar/Navbar.jsx
@@ -1,61 +1,49 @@
+import { Link, NavLink } from "react-router";
+
 const Navbar = () => {
+
+    const navbar = [
+        { link: "/consulting", menu: "Consulting" },
+        { link: "/learn", menu: "Learn" },
+        { link: "/clientSuccess", menu: "Client Success" },
+        { link: "/about", menu: "About" },
+        { link: "/contact", menu: "Contact" },
+    ]
+
+    const navbarClasses = ({ isActive }) => isActive ? "text-white" : ""
+
     return (
-        <nav>
-            <div className="navbar bg-base-100">
-                <div className="flex-1">
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
-                </div>
-                <div className="flex-none">
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                            <div className="indicator">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-5 w-5"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor">
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                                </svg>
-                                <span className="badge badge-sm indicator-item">8</span>
-                            </div>
+        <nav className="py-1">
+            <div className="navbar bg-base-100 xl:px-24">
+                <div className="flex justify-between">
+                    <div className="flex items-center">
+
+                        <div className="flex-1">
+                            <Link to={"/"} className="font-bold text-gray-500 text-3xl">Shahadad.</Link>
                         </div>
-                        <div
-                            tabIndex={0}
-                            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
-                            <div className="card-body">
-                                <span className="text-lg font-bold">8 Items</span>
-                                <span className="text-info">Subtotal: $999</span>
-                                <div className="card-actions">
-                                    <button className="btn btn-primary btn-block">View cart</button>
-                                </div>
-                            </div>
+
+                        <div className="ml-16">
+                            <ul className="flex">
+                                {/* <li className="text-bold text-black">Home</li> */}
+                                {
+                                    navbar.map((nav, index) => (
+                                        <NavLink
+                                            className={navbarClasses}
+                                            title={nav?.menu}
+                                            to={nav?.link}
+                                            key={index}
+                                            end
+                                        >
+                                            <h3 className="text-base font-semibold mx-4 hover:text-red-700">{nav?.menu}</h3>
+                                        </NavLink>
+                                    ))
+                                }
+                            </ul>
                         </div>
                     </div>
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img
-                                    alt="Tailwind CSS Navbar component"
-                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                            </div>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li>
-                                <a className="justify-between">
-                                    Profile
-                                    <span className="badge">New</span>
-                                </a>
-                            </li>
-                            <li><a>Settings</a></li>
-                            <li><a>Logout</a></li>
-                        </ul>
+
+                    <div>
+
                     </div>
                 </div>
             </div>
