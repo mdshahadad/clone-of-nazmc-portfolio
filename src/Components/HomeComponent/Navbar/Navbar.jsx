29e29672272d3668b835a182199221828bd5bc34
mdshahadad <identity.shahadad@gmail.com>
2024-12-14 05:05:09 +0600

    Added dropdown menu for the Consulting and learn menu

--- src/Components/HomeComponent/Navbar/Navbar.jsx
+++ src/Components/HomeComponent/Navbar/Navbar.jsx
@@ -1,15 +1,50 @@
+import { IoIosArrowDown } from "react-icons/io";
 import { PiShoppingCartLight } from "react-icons/pi";
 import { Link, NavLink } from "react-router";
+import './Navbar.css'
 
 const Navbar = () => {
 
+    // const navbar = [
+    //     { link: "/consulting", menu: "Consulting", icon: <IoIosArrowDown /> },
+    //     { link: "/learn", menu: "Learn", icon: <IoIosArrowDown /> },
+    //     { link: "/clientSuccess", menu: "Client Success" },
+    //     { link: "/about", menu: "About" },
+    //     { link: "/contact", menu: "Contact" },
+    // ]
+
     const navbar = [
-        { link: "/consulting", menu: "Consulting" },
-        { link: "/learn", menu: "Learn" },
+        {
+            link: "/consulting",
+            menu: "Consulting",
+            icon: <IoIosArrowDown />,
+            submenu: [
+                { link: "/consulting/strategy", menu: "Marketing Audit and Strategy" },
+                { link: "/consulting/operations", menu: "Marketing Advisor" },
+                { link: "/consulting/technology", menu: "Team Building" },
+                { link: "/consulting/technology", menu: "Part Time CMO" },
+            ],
+        },
+        {
+            link: "/learn",
+            menu: "Learn",
+            icon: <IoIosArrowDown />,
+            submenu: [
+                { link: "/learn/web-development", menu: "Freelancing Course - Paid" },
+                { link: "/learn/design", menu: "Freelancing Course - Paid" },
+                { link: "/learn/marketing", menu: "Freelancing Course - Paid" },
+                { link: "/learn/marketing", menu: "Freelancing Course - Paid" },
+                { link: "/learn/marketing", menu: "Freelancing Course - Paid" },
+                { link: "/learn/marketing", menu: "Freelancing Course - Paid" },
+                { link: "/learn/marketing", menu: "Freelancing Course - Paid" },
+                { link: "/learn/marketing", menu: "Freelancing Course - Paid" },
+            ],
+        },
         { link: "/clientSuccess", menu: "Client Success" },
         { link: "/about", menu: "About" },
         { link: "/contact", menu: "Contact" },
-    ]
+    ];
+
 
     const navbarClasses = ({ isActive }) => isActive ? "text-white" : ""
 
@@ -24,8 +59,39 @@ const Navbar = () => {
                         </div>
 
                         <div className="xl:ml-24">
-                            <ul className="flex">
-                                {/* <li className="text-bold text-black">Home</li> */}
+                            <ul className="flex items-center space-x-6">
+                                {navbar.map((nav, index) => (
+                                    <li key={index} className="relative group">
+                                        {/* Parent Menu Item */}
+                                        <NavLink
+                                            href={nav.link}
+                                            className="text-gray-800 font-medium hover:text-red-500 flex items-center gap-1"
+                                        >
+                                            {nav.menu}
+                                            {nav.icon && <span>{nav.icon}</span>}
+                                        </NavLink>
+
+                                        {/* Dropdown Menu */}
+                                        {nav.submenu && (
+                                            <ul className="absolute left-0 hidden group-hover:flex flex-col bg-white shadow-lg rounded-md py-2 w-48 z-10">
+                                                {nav.submenu.map((subItem, subIndex) => (
+                                                    <li key={subIndex}>
+                                                        <NavLink
+                                                            href={subItem.link}
+                                                            className="block px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-red-500"
+                                                        >
+                                                            {subItem.menu}
+                                                        </NavLink>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        )}
+                                    </li>
+                                ))}
+                            </ul>
+
+
+                            {/* <ul className="flex">
                                 {
                                     navbar.map((nav, index) => (
                                         <NavLink
@@ -35,11 +101,11 @@ const Navbar = () => {
                                             key={index}
                                             end
                                         >
-                                            <h3 className="text-base font-semibold mx-4 hover:text-red-700">{nav?.menu}</h3>
+                                            <h3 className="text-lg font-semibold mx-4 hover:text-red-700 flex items-center gap-2">{nav?.menu} <span className="mt-1">{nav?.icon}</span></h3>
                                         </NavLink>
                                     ))
                                 }
-                            </ul>
+                            </ul> */}
                         </div>
                     </div>
                     <div>
